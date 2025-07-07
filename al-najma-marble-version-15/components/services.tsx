9c412c96b637eada318321a5d535563e87b1a79f
Mehran Khan <139768831+mehrankhan909@users.noreply.github.com>
2025-07-07 17:35:07 +0500

    Update services.tsx

--- al-najma-marble-version-15/components/services.tsx
+++ al-najma-marble-version-15/components/services.tsx
@@ -422,7 +422,7 @@ export default function Services() {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 md:gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4 md:gap-6">
         {services.map((service, index) => (
           <div
             key={service.id}
